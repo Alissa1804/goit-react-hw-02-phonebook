c169a784288c612ddab945cdc104e8e7cc06e379
Alissa <alissa.werbach@live.de>
2022-11-28 12:14:38 +0100

    update color

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -6,7 +6,7 @@ export const Item = styled.li`
   flex-grow: 1;
   align-items: baseline;
   gap: 10px;
-  color: #baa287;
+  color: #cebdab;
   font-weight: 400;
   font-size: 20px;
   border-bottom: 1px solid #bdd4f1;
